5a0612d8e503d7e9116a6660f19922e14e719fb4
Dominik Guzei <dominik.guzei@gmail.com>
2015-10-07 15:40:42 +0200

    bumps version to 1.7.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Messaging infrastructure for Space applications.',
   name: 'space:messaging',
-  version: '1.7.1',
+  version: '1.7.2',
   git: 'https://github.com/meteor-space/messaging.git',
 });
 
