0c15c1622e1c80f0655ec50620d50fd5e8150517
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-01-30 01:18:32 +1100

    Add test for Space.messaging.EventSubscribing.on

--- tests/unit/mixins/event-subscribing.tests.js
+++ tests/unit/mixins/event-subscribing.tests.js
@@ -10,24 +10,35 @@ describe("Space.messaging.EventSubscribing", function() {
     {}
   );
 
-  beforeEach(function() {
-    this.myClassInstance = new MyClass({
-      meteor: Meteor,
-      eventBus: EventBus,
-      underscore: _
-    });
-    this.myEventInstance = new MyEvent();
-  });
-
   it("does not provide empty default", function() {
     expect(MyClass.prototype.eventSubscriptions).not.to.exist;
   });
 
-  it("can handle events when handler functions are subscribed for the type", function() {
-    const handler = sinon.spy();
-    this.myClassInstance.subscribe(MyEvent, handler);
-    expect(this.myClassInstance.canHandleEvent(this.myEventInstance)).to.be.true;
+  describe("handling events", function(){
+
+    beforeEach(function() {
+      this.myClassInstance = new MyClass({
+        meteor: Meteor,
+        eventBus: EventBus,
+        underscore: _
+      });
+      this.myEventInstance = new MyEvent();
+    });
+
+    it("can handle events when handler functions are subscribed for the type", function() {
+      const handler = sinon.spy();
+      this.myClassInstance.subscribe(MyEvent, handler);
+      expect(this.myClassInstance.canHandleEvent(this.myEventInstance)).to.be.true;
+    });
+
+    it("calls the handler when passed an event it can handle", function() {
+      const handler = sinon.spy();
+      this.myClassInstance.subscribe(MyEvent, handler);
+      this.myClassInstance.on(this.myEventInstance);
+      expect(handler).to.have.been.called;
+    });
   });
+
 
   it("does not throw error if no handlers have been defined", function() {
     const createWithoutHandlers = function() {
