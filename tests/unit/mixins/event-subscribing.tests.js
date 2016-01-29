196a2b5c32e105fe64322d83120bf5cac74e4673
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-01-30 01:04:01 +1100

    Strengthen test

--- tests/unit/mixins/event-subscribing.tests.js
+++ tests/unit/mixins/event-subscribing.tests.js
@@ -3,12 +3,8 @@ describe("Space.messaging.EventSubscribing", function() {
 
   const EventBus = new Space.messaging.EventBus;
   const EventSubscribing = Space.messaging.EventSubscribing;
-  EventSubscribing.meteor = Meteor;
-  EventSubscribing.eventBus = EventBus;
-  EventSubscribing.underscore = _;
 
   const MyClass = Space.Object.extend({ mixin: EventSubscribing });
-
   const MyEvent = Space.messaging.Event.extend(
     'Space.messaging.EventSubscribing.__Test__.MyEvent',
     {}
@@ -20,9 +16,14 @@ describe("Space.messaging.EventSubscribing", function() {
 
   it("can handle events when handler functions are subscribed for the type", function() {
     const handler = sinon.spy();
-    MyClass.prototype.subscribe(MyEvent, handler);
+    const myClassInstance = new MyClass({
+      meteor: Meteor,
+      eventBus: EventBus,
+      underscore: _
+    });
+    myClassInstance.subscribe(MyEvent, handler);
     const myEventInstance = new MyEvent();
-    expect(MyClass.prototype.canHandleEvent(myEventInstance)).to.be.true;
+    expect(myClassInstance.canHandleEvent(myEventInstance)).to.be.true;
   });
 
   it("does not throw error if no handlers have been defined", function() {
