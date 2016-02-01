4529e2337cacbc4cc5ca1aca449f9b558e7b16bf
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-02-01 21:34:43 +1000

    Make Space.Error versionable

--- source/serializables/error.js
+++ source/serializables/error.js
@@ -1,3 +1,4 @@
-// Make errors serializable
+// Make errors serializable and versionable
 Space.Error.mixin(Space.messaging.Ejsonable);
 Space.Error.type('Space.Error');
+Space.messaging.mixin(Space.messaging.Versionable)
