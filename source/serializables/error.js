08265532d7e0c28ab30bf50f1a24df020efe99f2
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-02-13 01:43:09 +1100

    Add schemaVersion field to Space.Error
    Also fixes error with mixing

--- source/serializables/error.js
+++ source/serializables/error.js
@@ -1,4 +1,11 @@
-// Make errors serializable and versionable
+// Make errors serializable
 Space.Error.mixin(Space.messaging.Ejsonable);
 Space.Error.type('Space.Error');
-Space.messaging.mixin(Space.messaging.Versionable)
+
+// Make errors versionable
+Space.Error.mixin(Space.messaging.Versionable);
+const fields = Space.Error.prototype.fields.call(this);
+Space.Error.prototype.fields = function() {
+  fields.schemaVersion = Match.Optional(Match.Integer);
+  return fields;
+};
