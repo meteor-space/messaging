4af032b4563efd709f1a79199e62d92c71f0ac55
Dominik Guzei <dominik.guzei@gmail.com>
2016-02-14 21:58:45 -0500

    fixes currying of error fields method

--- source/serializables/error.js
+++ source/serializables/error.js
@@ -4,8 +4,10 @@ Space.Error.type('Space.Error');
 
 // Make errors versionable
 Space.Error.mixin(Space.messaging.Versionable);
-const fields = Space.Error.prototype.fields.call(this);
-Space.Error.prototype.fields = function() {
-  fields.schemaVersion = Match.Optional(Match.Integer);
-  return fields;
-};
+Space.Error.prototype.fields = _.wrap(Space.Error.prototype.fields,
+  function(original) {
+    const fields = original.call(this);
+    fields.schemaVersion = Match.Optional(Match.Integer);
+    return fields;
+  }
+);
